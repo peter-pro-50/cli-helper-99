c2296d4355e738db1ef1cf769259c2fed2bab005
peter-pro-50 <17111976.phuong@gmail.com>
2026-07-24 09:10:22 +0300

    add refactor logger implementation

--- logger.py
+++ logger.py
@@ -1,17 +1,32 @@
 import logging
-from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file='app.log', max_bytes=10485760, backup_count=5):
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    return logger
+class Logger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.configure_logging()
 
-# Example usage
+    def configure_logging(self):
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.DEBUG)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
+
 if __name__ == '__main__':
-    logger = setup_logger()
-    logger.info('Logger is set up!')
-    logger.error('This is an error message.')
+    log = Logger('MyLogger')
+    log.info('Logger initialized successfully.')
