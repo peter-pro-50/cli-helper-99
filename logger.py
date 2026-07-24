f2df8ad67f1270eca92b584a1d7ec4b81d7f7ef8
peter-pro-50 <17111976.phuong@gmail.com>
2026-07-24 09:09:12 +0300

    add implement logger with rotation setup

--- logger.py
+++ logger.py
@@ -1,8 +1,8 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file='app.log', max_bytes=5*1024*1024, backup_count=3):
-    logger = logging.getLogger('rotating_logger')
+def setup_logger(log_file='app.log', max_bytes=10485760, backup_count=5):
+    logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -12,6 +12,6 @@ def setup_logger(log_file='app.log', max_bytes=5*1024*1024, backup_count=3):
 
 # Example usage
 if __name__ == '__main__':
-    log = setup_logger()
-    log.debug('This is a debug message.')
-    log.info('Logger setup completed successfully.')
+    logger = setup_logger()
+    logger.info('Logger is set up!')
+    logger.error('This is an error message.')
