79e1820bb223ae9772597d7a5017e35d11d833ac
peter-pro-50 <17111976.phuong@gmail.com>
2026-08-07 09:10:58 +0300

    fix class restructuring and cleanup

--- core.py
+++ core.py
@@ -1,28 +1,32 @@
-def main_loop():
-    while True:
-        user_input = input('Enter a command: ')
-        if validate_input(user_input):
-            process_command(user_input)
-        else:
-            print('Invalid input. Please try again.')
+# core.py
+import os
+import json
 
+class FileManager:
+    def __init__(self, base_path):
+        self.base_path = base_path
 
-def validate_input(user_input):
-    valid_commands = ['start', 'stop', 'status', 'exit']
-    return user_input in valid_commands
+    def read_file(self, filename):
+        full_path = os.path.join(self.base_path, filename)
+        with open(full_path, 'r') as file:
+            return file.read()
 
+    def write_file(self, filename, data):
+        full_path = os.path.join(self.base_path, filename)
+        with open(full_path, 'w') as file:
+            file.write(data)
 
-def process_command(command):
-    if command == 'start':
-        print('Starting...')
-    elif command == 'stop':
-        print('Stopping...')
-    elif command == 'status':
-        print('Current status...')
-    elif command == 'exit':
-        print('Exiting...')
-        exit(0)
+class JsonFileManager(FileManager):
+    def read_json(self, filename):
+        content = self.read_file(filename)
+        return json.loads(content)
 
+    def write_json(self, filename, data_dict):
+        json_data = json.dumps(data_dict, indent=4)
+        self.write_file(filename, json_data)
 
 if __name__ == '__main__':
-    main_loop()
+    fm = JsonFileManager('./data')
+    fm.write_json('sample.json', {'key': 'value'})
+    json_data = fm.read_json('sample.json')
+    print(json_data)
