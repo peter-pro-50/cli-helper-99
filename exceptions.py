d6cb21538234f8ce7a9bdeaa7b85cb6635f7dd44
peter-pro-50 <17111976.phuong@gmail.com>
2026-08-14 21:15:47 +0300

    fix custom error classes for validation

--- exceptions.py
+++ exceptions.py
@@ -1,28 +1,33 @@
 class CustomError(Exception):
     """Base class for other exceptions."""
-    def __init__(self, message):
-        super().__init__(message)
+    pass
+
+class ValueTooHighError(CustomError):
+    """Raised when the input value is too high."""
+    def __init__(self, value, message="Value is too high!"): 
+        self.value = value
         self.message = message
+        super().__init__(self.message)
 
-class ValidationError(CustomError):
-    """Raised when a validation check fails."""
-    def __init__(self, message):
-        super().__init__(message)
+class ValueTooLowError(CustomError):
+    """Raised when the input value is too low."""
+    def __init__(self, value, message="Value is too low!"):
+        self.value = value
+        self.message = message
+        super().__init__(self.message)
 
-class NotFoundError(CustomError):
-    """Raised when a resource is not found."""
-    def __init__(self, resource):
-        message = f'{resource} not found.'
-        super().__init__(message)
+class InvalidInputError(CustomError):
+    """Raised when the input is not valid."""
+    def __init__(self, value, message="Invalid input provided!"):
+        self.value = value
+        self.message = message
+        super().__init__(self.message)
 
-class PermissionDeniedError(CustomError):
-    """Raised when permission is denied."""
-    def __init__(self, action):
-        message = f'Permission denied for action: {action}'
-        super().__init__(message)
-
-class ConfigurationError(CustomError):
-    """Raised for configuration issues."""
-    def __init__(self, setting):
-        message = f'Configuration issue with setting: {setting}'
-        super().__init__(message)
+def validate_value(value):
+    if value < 10:
+        raise ValueTooLowError(value)
+    elif value > 100:
+        raise ValueTooHighError(value)
+    elif not isinstance(value, (int, float)):
+        raise InvalidInputError(value)
+    return True
