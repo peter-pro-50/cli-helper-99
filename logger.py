32742d0b8716dca683d9fc139140c8f36efc11a3
peter-pro-50 <17111976.phuong@gmail.com>
2026-08-17 23:14:34 +0300

    update logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,36 +1,14 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
+def setup_logger(name, log_file, level=logging.DEBUG):
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler = RotatingFileHandler(log_file, maxBytes=5*1024*1024, backupCount=2)
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+    return logger
 
-class CustomLogger:
-    def __init__(self, name: str, log_file: str, max_bytes: int = 5 * 1024 * 1024, backup_count: int = 3):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-        handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
-
-    def debug(self, message):
-        self.logger.debug(message)
-
-    def info(self, message):
-        self.logger.info(message)
-
-    def warning(self, message):
-        self.logger.warning(message)
-
-    def error(self, message):
-        self.logger.error(message)
-
-    def critical(self, message):
-        self.logger.critical(message)
-
-
-if __name__ == '__main__':
-    logger = CustomLogger('MyLogger', 'app.log')
-    logger.info('Logger setup complete.')
-    logger.debug('This is a debug message.')
-    logger.warning('This is a warning message.')
-    logger.error('This is an error message.')
-    logger.critical('This is a critical message.')
+logger = setup_logger('cli_helper', 'cli_helper.log')
+logger.info('Logger setup complete.')
